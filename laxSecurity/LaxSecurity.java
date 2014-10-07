52c313c1f12e586ba8e0dbaf7ce2a763b73ad5ea
Kaja Coraor <kaja.coraor@gmail.com>
2014-10-06 22:12:29 -0400

    Create LaxSecurity.java

---
+++ laxSecurity/LaxSecurity.java
@@ -0,0 +1,29 @@
+package main;
+
+public class LaxSecurity {
+
+	public static void main(String[] args) {
+		LaxSecurityHelper test = new LaxSecurityHelper();
+	}
+}
+
+/* SAMPLE INPUT
+10
+10360
+74288
+39491
+20457
+62973
+99999
+54385
+36491
+87873
+33899
+6
+54385
+48484
+62673
+20158
+28457
+36491
+*/
